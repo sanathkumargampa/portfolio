dd00bf510925eb47e39713dd4032dc6652c94b15
sanathkumargampa <sanathkumarsunny33@gmail.com>
2026-02-07 17:53:19 +0530

    vercel update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
